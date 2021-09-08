8fee428ba0171acefed724696a6c6cb00e5a0c0d
Sander Wieringa <s.wieringa@student.fontys.nl>
2021-09-08 10:18:46 +0200

    added table with mockup data and checkBoxes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,88 @@
 
 import './App.css';
 
-import SymbolList from './Components/SymbolList';
-import PersonList from './Components/PersonList';
+import * as React from 'react';
+import { DataGrid } from '@material-ui/data-grid';
 
-function App() {
+const columns = [
+  { field: 'id', headerName: 'ID', width: 100 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 160,
+    editable: true
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 160,
+    editable: true,
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true,
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column tada',
+    sortable: false,
+    width: 160,
+    valueGetter: (params: { getValue: (arg0: any, arg1: string) => any; id: any; }) =>
+    `${params.getValue(params.id, 'firstName') || ''} ${
+      params.getValue(params.id, 'lastName') || ''
+    }`
+  }
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 14 },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+];
+
+export default function DataTable() {
   return (
-    
-    <div className="App">
-      
-      <header className="App-header">
-        <SymbolList />
-      </header>
-      
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid 
+      rows={rows}
+      columns={columns}
+      pageSize={5}
+      checkboxSelection
+      disableSelectionOnClick
+      />
     </div>
-    
   );
-  
 }
 
-export default App;
+
+
+// function App() {
+//   const [data, setData] = useState([])
+//   return (
+
+    
+    
+//     <div className="App">
+      
+//       <header className="App-header">
+      
+//         <SymbolList />
+//       </header>
+      
+//     </div>
+    
+//   );
+  
+// }
+
+// export default App;
