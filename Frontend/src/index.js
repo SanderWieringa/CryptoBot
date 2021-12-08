f80268ba48d10fefec0cbd3838fa2ff514f5d088
Sander Wieringa <s.wieringa@student.fontys.nl>
2021-12-08 07:45:25 +0100

    removed datatable

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,7 +5,6 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import { SignIn } from "./SignIn";
 import { SignUp } from "./SignUp";
 import { ProtectedRoute } from "./Protected.route";
-import { DataTable } from "./DataTable";
 import { ProductTable } from "./Product.Table";
 import { Account } from "./Account";
 import { OrderTable } from "./OrderTable";
@@ -17,7 +16,6 @@ function App() {
         <Switch>
           <Route exact path="/" component={SignIn} />
           <Route exact path="/signup" component={SignUp} />
-          <ProtectedRoute exact path="/home" component={DataTable} />
           <ProtectedRoute exact path="/products" component={ProductTable} />
           <ProtectedRoute exact path="/account" component={Account} />
           <ProtectedRoute exact path="/orders" component={OrderTable} />
