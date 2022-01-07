f4d8f63283fc3abb764009013f1292e54635f2ba
Sander Wieringa <s.wieringa@student.fontys.nl>
2022-01-07 13:50:48 +0100

    added register and login tests

--- Backend/src/test/java/Rest/CryptoTradingBotApplicationTests.java
+++ Backend/src/test/java/Rest/CryptoTradingBotApplicationTests.java
@@ -4,6 +4,7 @@ import Rest.Controllers.BinanceController;
 import Rest.Controllers.UserController;
 import Rest.Entities.Product;
 import Rest.Entities.User;
+import Rest.Responses.AuthenticationRequest;
 import Rest.Responses.GetProductCollectionResponse;
 import Rest.Responses.OrderResponse;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,8 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CryptoTradingBotApplicationTests {
@@ -74,5 +77,40 @@ class CryptoTradingBotApplicationTests {
 		assertThat(entity.getBody().getOrders()).isNotNull();
 	}
 
+	@Test
+	public void testRegister() {
+		User user = new User(1, "asdf", "asdf", new ArrayList<>());
+		assertThat(userController.addUser(user).getBody().isSuccess()).isTrue();
+	}
 
+	@Test
+	public void testExistingRegister() {
+		User userOne = new User(1, "asdf", "asdf", new ArrayList<>());
+		assertThat(userController.addUser(userOne).getBody().isSuccess()).isTrue();
+		User userTwo = new User(2, "asdf", "asdf", new ArrayList<>());
+		assertThat(userController.addUser(userTwo).getBody().isSuccess()).isFalse();
+	}
+
+	@Test
+	public void testCorrectLogin() {
+		User userOne = new User(1, "asdf", "asdf", new ArrayList<>());
+		assertThat(userController.addUser(userOne).getBody().isSuccess()).isTrue();
+		AuthenticationRequest authenticationRequest = new AuthenticationRequest(userOne.getUsername(), userOne.getPassword());
+		assertThat(userController.createAuthenticationToken(authenticationRequest)).isNotNull();
+		assertThat(userController.createAuthenticationToken(authenticationRequest)).isNotNull();
+	}
+
+	@Test
+	public void testSubCoin() {
+		List<Product> products = new ArrayList<>();
+
+		User user = new User(1, "asdf", "asdf", products);
+		assertThat(userController.addUser(user).getBody().isSuccess()).isTrue();
+
+		products.add(new Product(1, "Bitcoin", 60000, 600000, "BTCBUSD"));
+
+		user.setCoinsToTradeIn(products);
+
+		assertThat(userController.update(user)).isNotNull();
+	}
 }
