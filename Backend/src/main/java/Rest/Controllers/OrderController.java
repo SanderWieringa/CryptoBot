0ddd056348bf2a826aa53362c4d661b97a9a2014
Sander Wieringa <s.wieringa@student.fontys.nl>
2022-01-22 16:32:58 +0100

    test 8 trigger

--- Backend/src/main/java/Rest/Controllers/OrderController.java
+++ Backend/src/main/java/Rest/Controllers/OrderController.java
@@ -57,7 +57,7 @@ public class OrderController {
 
         System.out.println("orderMessage.getSender: " + orderMessage.getSender());
 
-        System.out.println("jenkins8");
+        System.out.println("jenkins9");
 
         List<Order> allOrders = new ArrayList<>();
         Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("userId", orderMessage.getSender());
