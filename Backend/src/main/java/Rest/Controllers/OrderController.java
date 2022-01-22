ebfdf638b449c7da520cdc35fd54fd19e399aab5
Sander Wieringa <s.wieringa@student.fontys.nl>
2022-01-22 13:40:19 +0100

    jenkins trigger test 3

--- Backend/src/main/java/Rest/Controllers/OrderController.java
+++ Backend/src/main/java/Rest/Controllers/OrderController.java
@@ -57,7 +57,7 @@ public class OrderController {
 
         System.out.println("orderMessage.getSender: " + orderMessage.getSender());
 
-        System.out.println("jenkins2");
+        System.out.println("jenkins3");
 
         List<Order> allOrders = new ArrayList<>();
         Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("userId", orderMessage.getSender());
