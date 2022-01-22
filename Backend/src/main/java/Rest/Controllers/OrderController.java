94de7ed651a0824fa018e61bc73afb3baf6cde18
Sander Wieringa <s.wieringa@student.fontys.nl>
2022-01-22 14:16:22 +0100

    test 4 github webhook

--- Backend/src/main/java/Rest/Controllers/OrderController.java
+++ Backend/src/main/java/Rest/Controllers/OrderController.java
@@ -57,7 +57,7 @@ public class OrderController {
 
         System.out.println("orderMessage.getSender: " + orderMessage.getSender());
 
-        System.out.println("jenkins3");
+        System.out.println("jenkins4");
 
         List<Order> allOrders = new ArrayList<>();
         Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("userId", orderMessage.getSender());
